bda9ef4df65f6ecfaa31263ab5b8bfe8827cbe9d
subahan20 <129088945+subahan20@users.noreply.github.com>
2023-05-13 16:40:12 +0530

    Add files via upload

---
+++ models/question.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+// create the Question Schema
+const QuestionSchema = new mongoose.Schema({
+
+    id: {
+        type: Number,
+        required: true
+    },
+    title: {
+        type: String,
+        required: true
+    },
+    // include the array of id of all options in the question Schema
+    options: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Option'
+        }
+    ]
+
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('Question', QuestionSchema);
